653e5d08882105d63fa648300745f16d918ea095
Luciano Matos <luciano7matos@gmail.com>
2022-11-08 22:32:39 -0300

    integração bd mysql

--- layoutstore/settings.py
+++ layoutstore/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'layoutstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'db_layout',
+        'USER': 'root',
+        'PASSWORD': '643921',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
